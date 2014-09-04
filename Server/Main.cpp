ebdd2a6b2085b64ca11e342c0cbad10b049c89c3
activatedgeek <ksanyam18@yahoo.com>
2014-09-05 02:35:56 +0530

    Runnable main file

---
+++ Server/Main.cpp
@@ -0,0 +1,33 @@
+/*
+ * The main executable for the Chat Application Server
+ */
+
+/* Application Includes */
+#include "Server.h"
+
+int ATOI(char *num){
+	int n = strlen(num);
+	int res=0,place=1,i=n-1;
+	while(i>=0){
+		if(num[i]>='0' && num[i]<='9'){
+			res += int(num[i]-'0')*place;
+			--i;
+			place*=10;
+		}else{
+			return -1;
+		}
+	}
+	return res;
+}
+
+int main(int argc, char* argv[]){
+	int port = -1;
+	if (argc<2){
+		std::cout<<"WARNING: No port specified, selecting random port.\n";
+	}else{
+		port = ATOI(argv[1]);
+	}
+	startServer(port);
+
+	return 0;
+}
