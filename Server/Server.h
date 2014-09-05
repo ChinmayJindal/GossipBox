de2bbb9162bf46512e49a52f753fc09e2867226d
activatedgeek <ksanyam18@yahoo.com>
2014-09-05 20:20:06 +0530

    QUERY type handled, table entries working.

--- Server/Server.h
+++ Server/Server.h
@@ -24,7 +24,8 @@
 #define E_ACCEPT 504					/* Error accepting connection */
 #define E_RECV 505						/* Error in receive (recv) */
 #define E_HUNG 506						/* Error connection closed */
-#define E_IPADDR 507					/* Error retreiving IP address */
+#define E_SEND 507						/* Error in sending message */
+#define E_IPADDR 508					/* Error retreiving IP address */
 
 #define TYPE_QUERY 100
 #define TYPE_SEND 101
@@ -40,9 +41,11 @@ extern std::string error_message;		/* Error message set here */
 
 /* Tables to manage client info */
 typedef std::pair<std::string,int> iportPair;					/* This contains IP address, Port pair */
+typedef std::map<int, iportPair> SocketTable;
+typedef std::map<std::string, iportPair> NameTable;
 
-extern std::map<int, iportPair> socketTable;					/* Table with key as socket descriptor */
-extern std::map<std::string, iportPair> nameTable;				/* Table with key as username of client */
+extern SocketTable socketTable;					/* Table with key as socket descriptor */
+extern NameTable nameTable;						/* Table with key as username of client */
 
 /* 
  * Returns socket descriptor on success, else -1 and sets error flag	
@@ -50,31 +53,30 @@ extern std::map<std::string, iportPair> nameTable;				/* Table with key as usern
  */
 int setupSocket(int);	
 
-/*
- * Start the server's loop, using non-blocking interface
- */				
+/* Start the server's loop, using non-blocking interface */				
 void startServer(int);	
 
-/*
- *	Accept connections, update hashmap
- */				
+/* Accept connections, update hashmap */				
 int acceptConnection(int);
 
-/*
- * Receive data, set error flags (if any)
- */		
+/* Receive data, set error flags (if any) */		
 int receiveData(int, char*);
 
-/*
- * Process request,
- */
-void processRequest(char*);
+/* Process request */
+void processRequest(int, char*);
 
-/*
- * Split data, based on delimiters, upto a limit
- */
+/* Split data, based on delimiters, upto a limit */
 void splitCharStream(char*, const char*, int, std::vector<std::string>*);
 void trim(std::string&, const char);
 
+/* Function for table entries, queries */
+void addToSocketTable(int, std::string, int);
+void addToNameTable(std::string, std::string, int);
+iportPair getFromSocketTable(int);
+iportPair getFromNameTable(std::string);
+
+/* Get latest online nicknames */
+std::string getOnlineList(std::string);
+bool isOnline(std::string);
 
 #endif		//_SERVER_H_
