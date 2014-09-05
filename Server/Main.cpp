78ee47d5c4396f1f2d133a554df26577d61f44f3
activatedgeek <ksanyam18@yahoo.com>
2014-09-05 22:58:28 +0530

    Added signal handlers, closing socket on forceful termination

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -5,6 +5,35 @@
 /* Application Includes */
 #include "Server.h"
 
+/* System includes */
+#include <stdlib.h>
+#include <signal.h>
+
+/* Forward declarations */
+int ATOI(char*);										/* Convert char array into integer */
+void signal_callback_handler(int);						/* Handle interrupt signals */
+
+int main(int argc, char* argv[]){
+	signal(SIGINT, signal_callback_handler);
+	signal(SIGTERM, signal_callback_handler);
+	signal(SIGTSTP, signal_callback_handler);
+
+	int port = PORT_DEFAULT;
+	if (argc<2){
+		std::cout<<"WARNING: No port specified, selecting default port: "<<PORT_DEFAULT<<"\n";
+	}else{
+		port = ATOI(argv[1]);
+		if(port==-1){
+			std::cout<<"WARNING: No port specified, selecting default port: "<<PORT_DEFAULT<<"\n";
+			port = PORT_DEFAULT;
+		}
+	}
+
+	startServer(port);
+
+	return 0;
+}
+
 int ATOI(char *num){
 	int n = strlen(num);
 	int res=0,place=1,i=n-1;
@@ -20,15 +49,8 @@ int ATOI(char *num){
 	return res;
 }
 
-int main(int argc, char* argv[]){
-	int port = PORT_DEFAULT;
-	if (argc<2){
-		std::cout<<"WARNING: No port specified, selecting default port: "<<PORT_DEFAULT<<"\n";
-	}else{
-		port = ATOI(argv[1]);
-	}
-
-	startServer(port);
-
-	return 0;
+void signal_callback_handler(int signum){
+	std::cout<<"\nEnding services..\n";
+	close(SERVER_SOCKET);
+	exit(0);
 }
