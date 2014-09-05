906f0a1a0e92c2b0953fd8f3422cbe97e54011d1
activatedgeek <ksanyam18@yahoo.com>
2014-09-05 16:54:15 +0530

    added new hashtables, new utility methods

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,19 +2,23 @@
 
 /* Global variable definitions, declared in "Server.h" */
 int ERROR;
-//string error_message;
+std::string error_message;
+
+std::map<int, iportPair> socketTable;
+std::map<std::string, iportPair> nameTable;
 
 int setupSocket(int port){
 	int sd = socket(AF_INET, SOCK_STREAM, 0);				/* Socket Descriptor */
 	if(sd<0){
 		ERROR = E_DESCRIPTOR;
-		//error_message = "Error creating socket.";
+		error_message = "Error creating socket.";
 		return -1;
 	}
 
 	/* Standard Parameters */
 	struct sockaddr_in servAddr;
 	memset(&servAddr, 0, sizeof(servAddr));
+	
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	if(port==-1){
@@ -26,7 +30,7 @@ int setupSocket(int port){
 	int b = bind(sd, (struct sockaddr *) &servAddr, sizeof(servAddr));
 	if(b<0){
 		ERROR = E_BIND;
-		//error_message = "Error binding socket.";
+		error_message = "Error binding socket.";
 		return -1;
 	}
 
@@ -34,12 +38,12 @@ int setupSocket(int port){
 	int l = listen(sd, MAX_PENDING);
 	if(l<0){
 		ERROR = E_LISTEN;
-		//error_message = "Error setting listener.";
+		error_message = "Error setting listener.";
 		return -1;
 	}
 
 	ERROR = SOCKET_OK;
-	//error_message = "";
+	error_message = "";
 	
 	return sd;
 }
@@ -58,9 +62,9 @@ void startServer(int port){
 	fd_set read_sds;								/* Temporary socket descriptor set */
 	FD_ZERO(&master);
 	FD_ZERO(&read_sds);
-	struct sockaddr_in remoteAddr;					/* For connected client parameters */
+
 	int maxsd;										/* Maximum descriptor number */
-	int buffer[MAX_BUFFER];							/* Buffer for incoming data storage */
+	char buffer[MAX_BUFFER];						/* For incoming data storage */
 
 	/* Add current socket to master set */
 	FD_SET(socketDescriptor, &master);
@@ -75,39 +79,150 @@ void startServer(int port){
 		int s = select(maxsd+1, &read_sds, NULL, NULL, &timeOut);
 		if(s<0){
 			ERROR = E_SELECT;
-			//error_message = "Error using select.";
+			error_message = "Error using select.";
 		}
 
 		/* Search for data to read */
 		for(int sock=0;sock<=maxsd;++sock){
-			if(FD_ISSET(sock, &read_sds)){			/* Pending read status found */
-				if(sock == socketDescriptor){		/* If match on socket descriptor of server */
-					socklen_t remoteAddrLen = sizeof(remoteAddr);
-					int newSd = accept(socketDescriptor, (struct sockaddr*)&remoteAddr, &remoteAddrLen);
+			if(FD_ISSET(sock, &read_sds)){									/* Pending read status found */
+				if(sock == socketDescriptor){								/* If match on socket descriptor of server */
+					int newSd = acceptConnection(socketDescriptor);
 					if(newSd>-1){
 						FD_SET(newSd, &master);
-						maxsd = std::max(maxsd, newSd);	/* Update maximum descriptor */
+						maxsd = std::max(maxsd, newSd);						/* Update maximum descriptor */
 					}
 				}
-			}
-			else{									/* Handle data from client */
-				int nbytes = recv(sock, buffer, sizeof(buffer), 0);
-				if(nbytes<=0){
-					if(nbytes==0){
-						ERROR = E_CONN_CLOSED;
-						//error_message = "Connection hung up.";
+				else{														/* Handle data from client */
+					int nbytes = receiveData(sock, buffer);
+					if(nbytes==-1){
+						close(sock);
+						FD_CLR(sock, &master);								/* Update the master socket descriptor set */
 					}
-					else{
-						ERROR = E_RECV;
-						//error_message = "Error receiving data.";
-					}
-					close(sock);
-					FD_CLR(sock, &master);
-				}
-				else{
-					//Send data to clients here
+					processRequest(buffer);
 				}
 			}
 		}
 	}
+}
+
+int acceptConnection(int serverSocket){
+	struct sockaddr_in remoteAddr;											/* IP address and Port info */
+	socklen_t remoteAddrLen = sizeof(struct sockaddr_in);
+	memset(&remoteAddr, 0, remoteAddrLen);
+
+	int newSd = accept(serverSocket, (struct sockaddr*)&remoteAddr, &remoteAddrLen);
+	
+	if(newSd<0){
+		ERROR = E_ACCEPT;
+		error_message = "Failed connection accept.";
+		return -1;
+	}
+
+	char remoteIP[INET_ADDRSTRLEN];
+	if(inet_ntop(AF_INET, &remoteAddr.sin_addr.s_addr, remoteIP, sizeof(remoteIP)) == NULL){
+		ERROR = E_IPADDR;
+		error_message = "Error retrieving IP address of client.";
+		return -1;
+	}
+	int remotePort = ntohs(remoteAddr.sin_port);
+
+	//TO-DO add to hashtable
+
+	return newSd;
+}
+
+int receiveData(int socketDescriptor,char* buffer){
+	//memset(buffer, '\0', MAX_BUFFER);
+
+	int nbytes = recv(socketDescriptor, buffer, sizeof(buffer), 0);
+	if(nbytes<=0){
+		if(nbytes==0){
+			ERROR = E_HUNG;
+			error_message = "Connection hung up.";
+		}
+		else{
+			ERROR = E_RECV;
+			error_message = "Error receiving data.";
+		}
+		return -1;
+	}
+	ERROR = SOCKET_OK;
+	error_message = "";
+	return nbytes;
+}
+
+/*
+ * Parse incoming data
+ * Types of messages and their responses are:
+ * 					QUERY: <from>
+ *						Response --> ONLINE: <user1> : <user2> : <user3> : ....
+ *					SEND: <from> : <to> : <message>
+ *						Response --> SENT: <to> : <statusMessage>
+ */
+void processRequest(char* stream){
+	std::vector<std::string> tokens;
+
+	char* dump = strdup(stream);												/* Create a duplicate stream */
+	splitCharStream(dump, DELIM, 1, &tokens);									/* Retrieve the kind of message */
+	delete dump;
+
+	std::string reqType = tokens[0];
+	int TYPE_FLAG = (reqType=="QUERY")? TYPE_QUERY : TYPE_SEND;
+	int delimCount = (TYPE_FLAG==TYPE_QUERY)? 0 : 2;
+
+	splitCharStream(strdup(tokens[1].c_str()), DELIM, delimCount, &tokens);		/* Based on kind retrieve other param list */
+	
+	std::string fromUser = tokens[0];
+	switch(TYPE_FLAG){
+		case TYPE_QUERY:
+			
+			break;
+		case TYPE_SEND:
+			
+			break;
+		default:
+			break;
+	}
+}
+
+/*
+ * Split routine, 'count' defines the number of delimiters to check
+ */
+void splitCharStream(char* stream, const char* delim, int count, std::vector<std::string>* result){
+	(*result).clear();													/* Clear the result vector */
+
+	char* token;
+	std::string subToken;
+
+	/* Flag to check if limits applicable */
+	bool limitDelim = (count==-1)? false : true;
+
+	token = strtok(stream, delim);
+	while(token!=NULL){
+		if(limitDelim)													/* If limit, then update count */
+			--count;
+
+		subToken = std::string(token);
+		trim(subToken, ' ');											/* Trim leading and trailing spaces */
+		(*result).push_back(subToken);									/* Push into results */
+
+		if(limitDelim && count<=0)										/* Get all of next of count exhausted */
+			token = strtok(NULL, "");
+		else															/* Keep applying default delimiter */
+			token = strtok(NULL, delim);
+	}
+}
+
+/* 
+ * Trim leading and trailing unwanted char (tchar)
+ */
+void trim(std::string& s, const char tchar){
+	int i=0;
+	while(s[i]==tchar)
+		++i;
+	s = s.substr(i, std::string::npos);
+	i=s.length()-1;
+	while(s[i]==tchar)
+		--i;
+	s = s.substr(0, i+1);
 }
--- Server/Server.h
+++ Server/Server.h
@@ -6,12 +6,14 @@
 #include <string.h>
 #include <algorithm>
 #include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-
 /* Codes */
 #define SOCKET_OK 200					/* All OK */
 
@@ -21,22 +23,58 @@
 #define E_SELECT 503					/* Error using select() (not synchronous I/O) */
 #define E_ACCEPT 504					/* Error accepting connection */
 #define E_RECV 505						/* Error in receive (recv) */
-#define E_CONN_CLOSED 506				/* Error connection closed */
+#define E_HUNG 506						/* Error connection closed */
+#define E_IPADDR 507					/* Error retreiving IP address */
+
+#define TYPE_QUERY 100
+#define TYPE_SEND 101
 
 /* Required parameters */
 #define PORT_DEFAULT 1893				/* Select default port */
 #define MAX_PENDING 10					/* Maximum pending Requests */
 #define MAX_BUFFER 1000					/* Maximum buffer size for data */
+#define DELIM ":"						/* Delimiter for queries from client */
 
 extern int ERROR;						/* Error flags set here */
-//extern string error_message;			/* Error message set here */
+extern std::string error_message;		/* Error message set here */
 
-/* Function prototypes */
-int setupSocket(int);					/* Returns socket descriptor on success, else -1 and sets error flag	
-										 * If Port number -1, switch to default
-										 */
-void startServer(int);					/*
-										 * Start the server's loop, using non-blocking interface
-										 */
+/* Tables to manage client info */
+typedef std::pair<std::string,int> iportPair;					/* This contains IP address, Port pair */
+
+extern std::map<int, iportPair> socketTable;					/* Table with key as socket descriptor */
+extern std::map<std::string, iportPair> nameTable;				/* Table with key as username of client */
+
+/* 
+ * Returns socket descriptor on success, else -1 and sets error flag	
+ * If Port number -1, switch to default
+ */
+int setupSocket(int);	
+
+/*
+ * Start the server's loop, using non-blocking interface
+ */				
+void startServer(int);	
+
+/*
+ *	Accept connections, update hashmap
+ */				
+int acceptConnection(int);
+
+/*
+ * Receive data, set error flags (if any)
+ */		
+int receiveData(int, char*);
+
+/*
+ * Process request,
+ */
+void processRequest(char*);
+
+/*
+ * Split data, based on delimiters, upto a limit
+ */
+void splitCharStream(char*, const char*, int, std::vector<std::string>*);
+void trim(std::string&, const char);
+
 
 #endif		//_SERVER_H_
