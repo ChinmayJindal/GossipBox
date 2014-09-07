accff44e9b2a7777d84d309105330397ba8d1070
Chinmay Jindal <chinmayjindal1994@gmail.com>
2014-09-07 16:07:56 +0530

    while loop for receving

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -1,11 +1,10 @@
 #include "Client.h"
 
-
 int main(int argc, char **argv){
 
 	// checking the validity of args
 	if (argc < 2 || argc > 3){
-		cout << "<Server Address> <Server Port>";
+		std::cout << "<Server Address> <Server Port>";
 		return -1;
 	}
 
@@ -14,36 +13,47 @@ int main(int argc, char **argv){
 	in_port_t serverPort = (argc==3) ? atoi(argv[2]) : 1893;
 
 	// getting nickname from user
-	cout << "Enter your name: ";
+	std::cout << "Enter your name: ";
 	getline(std::cin, myname);
 
 	// get the socket descriptor for dealing with the connection
 	socketDescriptor = setupConnection(serverPort, serverIP);
 	if (socketDescriptor==-1)
 		return -1;
+	else
+		std::cout << "Connection to server established.\n";
 
+
+	sleep(1);
 	sendMessage(socketDescriptor, ("QUERY:"+myname).c_str());		// send username to server in format QUERY:username
 
-	receiveMessage(socketDescriptor, buffer, MAX_BUF_SIZE);			// Receive list of online clients from server
+	while(receiveMessage(socketDescriptor, buffer) <= 0);			// Receive list of online clients from server
 	
 	splitCharStream(strdup(buffer), DELIM, -1, &usersList);			// Display list of users
-	displayOnlineUsers(&usersList);
+	displayOnlineUsers(usersList);
+
+	sleep(2);
 
 	// giving instructions to user
-	cout << "To send a message to other users, enter the message in this format => username:message" << endl;
-	cout << "To query online users, enter who." << endl;
-	cout << "Enjoy Chatting." << endl;
+	std::cout << "To send a message to other users, enter the message in this format => username:message" << std::endl;
+	std::cout << "To query online users, enter who." << std::endl;
+	std::cout << "Enjoy Chatting." << std::endl;
+	
+
+	std::cin >> cmdInput;
+	
+
+	pthread_t threads[2];	
 	
 	// starting two threads
-	pthread_t threads[2];
 	int rc1 = pthread_create(&threads[0], NULL, Sender, (void *)1);
 	int rc2 = pthread_create(&threads[1], NULL, Receiver, (void *)2);
 	
 	if(rc1 || rc2)
-		cout << "Error: Unable to create thread";
+		std::cout << "Error: Unable to create thread\n";
 	
-	close(sd);
+	close(socketDescriptor);
 	pthread_exit(NULL);
-
+	
 	return 0;
 }
